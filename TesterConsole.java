bf8b1429d453a8be2ea979ad9d390082e553b0d7
Ouweshs28 <cst2550@localhost.localdomain>
2020-04-24 03:33:50 +0400

    code cleanup

--- TesterConsole.java
+++ TesterConsole.java
@@ -2,9 +2,6 @@ public class TesterConsole {
     public static void main(String[] args) {
         FileManagement songfile = new FileManagement();
         songfile.getFile("sample_song_data");
-        StopWatch create =new StopWatch();
-        StopWatch search =new StopWatch();
-        create.start();
         HashST<String, Song> song = new HashST<String, Song>();
         while (songfile.scan.hasNextLine()) {
             String line = songfile.scan.nextLine();
@@ -16,17 +13,7 @@ public class TesterConsole {
             Song p = new Song(title, artist, time, videofile);
             song.put(title, p); 
         }
-        System.out.println("Data structure created took " +create.stop() +" seconds");
-        search.start();
-        String seatchtitle="My Friend, the Dictionary";
-        System.out.println("Song found: "+song.get(seatchtitle));
-        if(song.get(seatchtitle)!=null){
-            Song searchsong=song.get(seatchtitle);
-
-            System.out.println(searchsong.getArtist());
+    
         }
-        System.out.println("search complete " +search.stop() +" seconds");
-        
-    }
 
 }
